a74c74f175cb1ea44c31d7d4632cd411bde52839
Alissanited <alissani09@gmail.com>
2021-04-18 22:08:49 -0700

    first

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import {Component} from 'react';
 import {useState} from 'react';
 import './App.css';
+import React from "react";
+
 
 
 const reuseComponet = 
   <div className="comptext">
     <br></br>
     <h2>Pet</h2>
-    <p>Name:</p>
-    <p>Breed:</p>
-    <p>Owner:</p>
+    <p>Name: </p>
+    <p>Breed: </p>
+    <p>Owner: </p>
+    <p>Fun facts: </p>
       <br></br>
     
 
@@ -25,14 +28,16 @@ function App() {
       <div className="compbox">{reuseComponet}</div>
       <div className="compbox">{reuseComponet}</div>
       <div className="compbox">{reuseComponet}</div>
+      <div className="compbox">{reuseComponet}</div>
     </div> 
     </div> 
-
-
-
-
 }
+
   
+
+
+
+
 
 
 export default App;
